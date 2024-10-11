c1f2cd1709dd82aeb44c3c1eb904be44f0f0c1da
Vineet Kumar <vk7913096@gmail.com>
2024-10-11 22:56:44 +0530

    Update Spinner.js

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,11 +1,21 @@
 // src/components/Spinner.js
 import React from "react";
-import { PropagateLoader } from "react-spinners";
+import { ColorRing } from "react-loader-spinner";
 import "../styles/Spinner.css";
 const Spinner = ({ loading }) => {
   return (
     <div className="spinner-container">
-      <PropagateLoader color="#3498db" loading={loading} size={50} />
+      <ColorRing
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="color-ring-loading"
+        wrapperStyle={{}}
+        wrapperClass="color-ring-wrapper"
+        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+        loading={loading}
+        size={50}
+      />{" "}
     </div>
   );
 };
