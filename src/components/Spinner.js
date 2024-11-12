db574c8ed61827073da9a15dc9d3952285474b94
Vineet Kumar <vk7913096@gmail.com>
2024-11-12 20:43:16 +0530

    Update Spinner.js

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,23 +1,8 @@
-// src/components/Spinner.js
 import React from "react";
-import { ColorRing } from "react-loader-spinner";
-import "../styles/Spinner.css";
-const Spinner = ({ loading }) => {
-  return (
-    <div className="spinner-container">
-      <ColorRing
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="color-ring-loading"
-        wrapperStyle={{}}
-        wrapperClass="color-ring-wrapper"
-        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
-        loading={loading}
-        size={50}
-      />{" "}
-    </div>
-  );
+import "../styles/Spinner.css"; // Make sure to include the CSS for the spinner
+
+const Spinner = () => {
+  return <div class="loader"></div>;
 };
 
 export default Spinner;
